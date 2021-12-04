963f329d1fafae3c759664c4653e1adcac504cad
toan tran <tpt109@gmail.com>
2021-12-04 23:30:46 +0700

    create jpa service

--- pet-clinic-data/src/main/java/sfgpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/sfgpetclinic/services/map/VisitMapService.java
@@ -1,10 +1,14 @@
 package sfgpetclinic.services.map;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 import sfgpetclinic.model.Visit;
 import sfgpetclinic.services.VisitService;
 
 import java.util.Set;
 
+@Service
+@Profile({"default", "map"})
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
     @Override
     public Set<Visit> findAll() {
